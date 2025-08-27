a2d2963216945baec78f31b38130a9c892c69b6a
ChaosCake4505 <48798914+ChaosCake4505@users.noreply.github.com>
2025-08-27 15:28:56 -0700

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,12 +4,13 @@ import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-// .eslintrc.json
 {
+  "extends": ["next/core-web-vitals"],
   "rules": {
     "react/no-unescaped-entities": ["error", { "forbid": [">", "}"] }]
   }
 }
+
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
