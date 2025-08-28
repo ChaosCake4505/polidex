c7f022b6e393d64ae04bd3cd9ccaffa9183ff56b
ChaosCake4505 <48798914+ChaosCake4505@users.noreply.github.com>
2025-08-27 22:10:14 -0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,15 @@
+// pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* optional: iOS address-bar color */}
+        <meta name="theme-color" content="#000000" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
